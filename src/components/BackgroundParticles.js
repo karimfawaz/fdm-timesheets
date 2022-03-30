b025692298e813447e23690569dcacbb22727a98
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-03-30 01:25:16 +0100

    Started DB Setup

--- src/components/BackgroundParticles.js
+++ src/components/BackgroundParticles.js
@@ -3,68 +3,65 @@ import Particles from "react-tsparticles"
 const BackgroundParticles = () => {
   return (
     <Particles
-    options={{
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
+      options={{
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+
+            resize: true,
+          },
+          modes: {
+
+            push: {
+              quantity: 2,
+            },
+
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
             enable: true,
-            mode: "push",
+            opacity: 0.5,
+            width: 1,
           },
-
-          resize: true,
-        },
-        modes: {
-
-          push: {
-            quantity: 2,
-          },
-
-        },
-      },
-      particles: {
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        // collisions: {
-        //   enable: true,
-        // },
-        move: {
-          direction: "none",
-          enable: true,
-          outMode: "bounce",
-          random: false,
-          speed: 6,
-          straight: false,
-        },
-        number: {
-          density: {
+          move: {
+            direction: "none",
             enable: true,
-            area: 1000,
+            outMode: "bounce",
+            random: false,
+            speed: 10,
+            straight: false,
           },
-          value: 100,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "star",
-          
-        },
-        size: {
-          random: true,
-          value: 5,
-        },
-      },
+          number: {
+            density: {
+              enable: true,
+              area: 1000,
+            },
+            value: 120,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "star",
 
-    }} />
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+
+      }} />
   )
 }
 
