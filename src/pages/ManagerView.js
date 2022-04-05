28c0c959ca9868bb152f868beeeca49c9038c5c5
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-04-06 00:35:32 +0100

    Started working on ManagerView

--- src/pages/ManagerView.js
+++ src/pages/ManagerView.js
@@ -1,15 +1,50 @@
 import office from "../assets/fdm-office.png"
 import Collapsible from "react-collapsible"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { DB, logout } from "../data/db"
+import BackgroundParticles from "../components/BackgroundParticles"
 
 import "../styles/ManagerView.css"
 
 const ManagerView = () => {
+
+  let navigate = useNavigate();
+  const [timesheets, setTimesheets] = useState(null);
+  function handleApprove(id) {
+    let item = timesheets.filter((item) => item.ID === id)[0];
+    item.status = "approved";
+    const newList = timesheets;
+    setTimesheets(newList);
+    DB.currentUser.timesheets = newList;
+
+  }
+
   return (
     <div className="managerPage">
+      <BackgroundParticles />
       <div className="manageTimesheets">
+        <img src={office} alt="office" className="officeImgManager" />
+        <div className="menuandtimesheets">
+          <div className="menu">
+          <div className="managerDetails">
+          <h3>Hey there, </h3>
+          <svg className="nameSvg">
+            <text className="svgText" x="50%" y="50%" fill="" textAnchor='middle'>
+              {DB.currentUser?.name}
+            </text>
+          </svg>
+          <h4> ID: {DB.currentUser?.ID}</h4>
+        </div>
+        <h3 onClick={() => { navigate("/"); logout() }} className="managerButton">
+            Logout
+          </h3>
+          </div>
+          <div className="managerTimesheets"></div>
+
+        </div>
 
       </div>
-      <img src={office} />
     </div>
   )
 }
