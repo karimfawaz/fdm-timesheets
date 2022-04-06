4f6ea2081d742a3b644b6a03de50388e81974fec
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-04-06 01:03:58 +0100

    Improving Manager View

--- src/pages/ManagerView.js
+++ src/pages/ManagerView.js
@@ -27,20 +27,35 @@ const ManagerView = () => {
         <img src={office} alt="office" className="officeImgManager" />
         <div className="menuandtimesheets">
           <div className="menu">
-          <div className="managerDetails">
-          <h3>Hey there, </h3>
-          <svg className="nameSvg">
-            <text className="svgText" x="50%" y="50%" fill="" textAnchor='middle'>
-              {DB.currentUser?.name}
-            </text>
-          </svg>
-          <h4> ID: {DB.currentUser?.ID}</h4>
-        </div>
-        <h3 onClick={() => { navigate("/"); logout() }} className="managerButton">
-            Logout
-          </h3>
+            <div className="managerDetails">
+              <h3>Hey there, </h3>
+              <svg className="nameSvg">
+                <text className="svgText" x="50%" y="50%" fill="" textAnchor='middle'>
+                  {DB.currentUser?.name}
+                </text>
+              </svg>
+              <h4> ID: {DB.currentUser?.ID}</h4>
+            </div>
+            <h3 onClick={() => { navigate("/"); logout() }} className="managerButton">
+              Logout
+            </h3>
           </div>
-          <div className="managerTimesheets"></div>
+          <div className="managerTimesheets">
+
+            
+            {DB.users.filter(user => user.type === "consultant").map((user, index) => (
+
+              <Collapsible trigger={<h3 className="managerCollapsibleHeader">{user.name}</h3>} className="managerCollapsibleDiv">
+                <div className="managerCollapsibleBody">
+jhkjhkjh
+                </div>
+              </Collapsible>
+
+
+            ))}
+
+
+          </div>
 
         </div>
 
